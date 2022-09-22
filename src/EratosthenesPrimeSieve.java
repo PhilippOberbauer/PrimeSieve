dab3cb68391cc8440b3bea48710e7033800c01a6
PhilippOberbauer <OberbauerP190207@sus.htl-grieskirchen.at>
2022-09-22 10:46:35 +0200

    Implemented PrimSieve in EratosthenesPrimeSieve

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,4 +1,53 @@
-public class EratosthenesPrimeSieve {
+import java.util.ArrayList;
+
+public class EratosthenesPrimeSieve implements PrimeSieve{
     private int limit;
-    
+    private ArrayList<Integer> primes;
+    private boolean[] marked;
+
+    public EratosthenesPrimeSieve(int limit) {
+        this.limit = limit;
+        primes = new ArrayList<>();
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        if (marked[p+2] == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public void printPrimes() {
+        for (int i = 0; i < primes.size(); i++)
+        {
+            System.out.println(primes.get(i));
+        }
+    }
+
+    public void primeSieve()
+    {
+        for (int i = 2; i < limit; i++)
+        {
+            marked[i] = false;
+        }
+
+        for (int i = 2; i < Math.sqrt(limit); i++)
+        {
+            if (!isPrime(i))
+            {
+                primes.add(i);
+
+                for (int j = i * i; j < limit; )
+                {
+                    marked[i] = true;
+                }
+            }
+        }
+    }
 }
