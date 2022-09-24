e1c2936bf1076f3be95bdfffe7a1d7c11dcf31d4
PhilippOberbauer <OberbauerP190207@sus.htl-grieskirchen.at>
2022-09-24 18:37:54 +0200

    Implemented main method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in, "Windows-1252");
+        scanner.useDelimiter("\n");
+        System.out.println("Bitte geben Sie die Obergrenze der zu berechnenden Primzahlen ein.");
+        int limit = scanner.nextInt();
+        EratosthenesPrimeSieve sieve = new EratosthenesPrimeSieve(limit);
+        sieve.primeSieve();
+        System.out.println("Primzahlen bis " + limit + ":");
+        sieve.printPrimes();
     }
 }
